ca787a1008b566bd5f1867f096fd70424f3badfe
Minh Chau <chau.ntm184238@sis.hust.edu.vn>
2020-05-23 17:28:02 +0700

    Initialize GUI with basic components

---
+++ Semiconductor/src/application/Main.java
@@ -0,0 +1,50 @@
+package application;
+
+import java.util.Optional;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+
+public class Main extends Application {
+
+	public void start(Stage primaryStage) {
+		try {
+			String strWelcome = "/application/WelcomeWindow.fxml";
+			Parent root = FXMLLoader.load(getClass().getResource(strWelcome));
+			primaryStage.setTitle("WelcomeWindow");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		primaryStage.setOnCloseRequest(e->{
+			Alert confirmation = new Alert(AlertType.CONFIRMATION);
+			confirmation.setHeaderText("Exit confirmation");
+			confirmation.setContentText("Are you sure you want to exit program?");
+			Optional<ButtonType> option = confirmation.showAndWait();
+			
+			if(option.get()==ButtonType.OK) {
+				confirmation.close();
+				Alert thank = new Alert(AlertType.INFORMATION);
+				thank.setHeaderText("Thank you very much!");
+				thank.setContentText("Thank you for using our software!");
+				thank.showAndWait();
+			} else {
+				e.consume();
+			}
+
+		});
+	}
+	
+	public static void main(String args[]) {
+		launch(args);
+	}
+
+}
